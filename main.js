c58b91280be8700790bae9b6a4318912c5495845
Josh Robertson <hossman333@gmail.com>
2014-09-05 15:26:38 -0600

    app

--- main.js
+++ main.js
@@ -1,37 +1,11 @@
-var startPosition = 0;
-var pagePosition = 0;
-var scrollY = 0;
-var scrollPrevented = false;
-
-$(document).on('vmousedown', '.dragme', function(event){
-    var startPosition = pagePosition;
-    $(document).on('vmousemove', function(event2){
-        scrollY = event2.pageY;
-        console.log(-scrollY);
-        $('.cover').css({
-            'top': -scrollY
-        })
-        if(scrollPrevented == false){
-          scrollPrevented = true;
-          $(document).on('touchmove', function(ev){
-            ev.preventDefault();
-          });            
-        }
+$(function() {
+    $(".dial").knob({
+        "fgColor":"#000",
+        "width":"200"
     });
+
+    $('.button').click(function() {
+        $(this).toggleClass('button-clicked');
+    })
+
 });
-
-$(document).on('vmouseup', function(){
-    if(scrollPrevented == true){
-        $('body').unbind('touchmove');
-        scrollPrevented = false;
-    }
-    $(document).off('vmousemove');
-});
-
-
-
-
-
-
-
-
